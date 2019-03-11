521a2a4b98e0f7235ade67df901e6b5f2326b337
Roman Bulinski <aboandrus@gmail.com>
2019-03-11 08:18:17 +0100

    Refactor names of tests

--- src/test/java/TestPerCheck.java
+++ src/test/java/TestPerCheck.java
@@ -7,42 +7,42 @@ public class TestPerCheck {
 
 
     @Test
-    public void testSolution(){
+    public void testRepetedNumber(){
         PermCheck permCheck = new PermCheck();
         int[] A = { 1,2,3,4,4 };
         assertEquals(  0,  permCheck.solution( A )  );
     }
 
     @Test
-    public void testSolution2(){
+    public void testSimpleCheck_1(){
         PermCheck permCheck = new PermCheck();
         int[] A = { 13,12,10,11,14 };
         assertEquals(  1,  permCheck.solution( A )  );
     }
 
     @Test
-    public void testSolution3(){
+    public void testSimpleCheck_0(){
         PermCheck permCheck = new PermCheck();
         int[] A = { 13,12,11,14,8 };
         assertEquals(  0,  permCheck.solution( A )  );
     }
 
     @Test
-    public void testSolution4(){
+    public void testArrayLength_2(){
         PermCheck permCheck = new PermCheck();
         int[] A = { 13,11 };
         assertEquals(  0,  permCheck.solution( A )  );
     }
 
     @Test
-    public void testSolution5(){
+    public void testArrayLength_1(){
         PermCheck permCheck = new PermCheck();
         int[] A = { 11 };
         assertEquals(  1,  permCheck.solution( A )  );
     }
 
     @Test
-    public void testSolution6(){
+    public void testREpetedNumbers(){
         PermCheck permCheck = new PermCheck();
         int[] A = { 10,11,11,11,11,15 };
         assertEquals(  0,  permCheck.solution( A )  );
